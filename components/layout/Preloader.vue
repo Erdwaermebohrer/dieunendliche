cf2f4e49f865eff4b4b00d6d3ba6b4727ecadeec
schnapsterdog <olivertrajceski@yahoo.com>
2021-09-16 13:11:49 +0200

    UE-8 Added Preloader Component with Transition, Added Static Video to Intro/Stage Component

---
+++ components/layout/Preloader.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="preloader__wrapper">
+    <div class="preloader__wrapper--video">
+      <video class="video" autoplay playsinline muted loop>
+        <source src="~static/intro-video.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  </div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="container">
+    <transition name="fade-preloader" mode="in-out">
+      <app-preloader v-if="pageLoading"/>
+    </transition>
     <app-header :inputData="header" />
     <Nuxt />
     <app-footer :inputData="footer" />
@@ -7,22 +10,31 @@
 </template>
 
 <script>
-import { mapGetters } from "vuex";
-import Header from '../components/layout/Header.vue';
-import Footer from '../components/layout/Footer.vue';
+import { mapActions, mapGetters } from "vuex";
+import Header from "../components/layout/Header.vue";
+import Footer from "../components/layout/Footer.vue";
+import Preloader from "../components/layout/Preloader.vue";
 
 export default {
   components: {
     "app-header": Header,
-    "app-footer": Footer
+    "app-footer": Footer,
+    "app-preloader": Preloader,
   },
   computed: {
-  ...mapGetters({
-      header: 'header',
-      footer: 'footer'
-    })
-}
+    ...mapGetters({
+      header: "header",
+      footer: "footer",
+      pageLoading: "pageLoading",
+    }),
+  },
+  methods: {
+    ...mapActions(["setPageLoading"]),
+  },
+  mounted() {
+    setTimeout(() => {
+      this.setPageLoading(false);
+    }, 1500);
+  },
 };
 </script>
-<style lang="scss">
-</style>
