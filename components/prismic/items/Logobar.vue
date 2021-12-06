a7b517b4fbb3d7a98115bc373fef7151d4a154a2
Jonathan <jonathanrobrecht@googlemail.com>
2021-12-06 09:35:08 +0100

    logobar

---
+++ components/prismic/items/Logobar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="logobar__wrapper">
+    <div class="logobar__wrapper--content">
+      <h2 class="title"  v-text="$prismic.asText(slice.primary.title)"/>
+
+      <div>
+      	<div class="description"  v-text="$prismic.asText(slice.primary.description)"/>
+
+      	<div class="logobar">
+	        <img class="image" :data-src="item.logo.url" v-for="(item, index) in slice.items" :key="'item-' + index" v-lazy-load/>
+	    </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    slice: {
+      type: Object,
+    },
+    clickedButton: {
+      type: Function,
+    },
+  },
+};
+</script>
