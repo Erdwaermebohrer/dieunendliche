d63d00e4f156f9f49f11eb739415240e96a74c92
SchnapsterDog <olivertrajceski@yahoo.com>
2023-02-23 00:08:32 +0100

    feat: adjusted contact module left part

--- components/prismic/items/ContactModule.vue
+++ components/prismic/items/ContactModule.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="contact-module__wrapper">
     <div class="contact-module__wrapper--left">
-      Left
+      <div class="title">
+        <headline-component :inputData="slice.primary.title" />
+      </div>
+      <div class="subtitle">
+        <headline-component :inputData="slice.primary.subtitle" />
+      </div>
     </div>
     <div class="contact-module__wrapper--right">
       Right
@@ -10,11 +15,15 @@
 </template>
 
 <script>
+import HeadlineComponent from "../custom/HeadlineComponent.vue";
 export default {
   props: {
     slice: {
       type: Object,
     }
+  },
+  components: {
+    HeadlineComponent
   },
 };
 </script>
