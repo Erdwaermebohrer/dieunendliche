f0501940845d36da26e50038f6d6a8aacce090ca
SchnapsterDog <olivertrajceski@yahoo.com>
2023-02-23 13:26:19 +0100

    fix: fixed blogStage link

--- components/prismic/items/BlogStage.vue
+++ components/prismic/items/BlogStage.vue
@@ -1,25 +1,39 @@
 <template>
-  <div :id="slice.primary.id" class="stage-component__wrapper">
-    <div class="stage-component__wrapper--left">
-      <div class="content__wrapper--bottom">
-        <div class="subtitle">
-          <headline-component :inputData="slice.primary.subtitle" />
-        </div>
-        <div class="title">
-          <headline-component :inputData="slice.primary.title" :className="'h3'"/>
-        </div>
-        <div
-          class="description"
-          v-html="$prismic.asHtml(slice.primary.description)"
-        />
-      </div>
-    </div>
-    <div class="stage-component__wrapper--right">
-      <div class="img">
-        <img v-lazy-load :data-src="slice.primary.image.url" :alt="slice.primary.image.alt" />
-      </div>
-    </div>
-  </div>
+	<div :id="slice.primary.id" class="stage-component__wrapper">
+		<div class="stage-component__wrapper--left">
+			<div class="content__wrapper--bottom">
+				<div class="subtitle">
+					<headline-component :inputData="slice.primary.subtitle" />
+				</div>
+				<div class="title">
+					<headline-component
+						:inputData="slice.primary.title"
+						:className="'h3'"
+					/>
+				</div>
+				<div
+					class="description"
+					v-html="$prismic.asHtml(slice.primary.description)"
+				/>
+        <nuxt-link
+          class="link"
+          :to="slice.primary.link.url"
+        >
+          <span>→</span>
+          <span>{{ $prismic.asText(slice.primary.link_title) }}</span>
+        </nuxt-link>
+			</div>
+		</div>
+		<div class="stage-component__wrapper--right">
+			<div class="img">
+				<img
+					v-lazy-load
+					:data-src="slice.primary.image.url"
+					:alt="slice.primary.image.alt"
+				/>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
